a37d3120b45291f030cb3b01bd701ad41386c006
NAILA ZEMMACHE <155105829+nailamadjid123@users.noreply.github.com>
2024-10-31 18:38:16 +0100

    first commit

---
+++ exercice1.py
@@ -0,0 +1,10 @@
+import math
+
+#une fonction qui calcule la quantité d'information 
+
+def quantité_information(probablité):
+    if probabilité > 0:
+        return -math.log2(probabilité)
+    else:
+        return 0 
+            
