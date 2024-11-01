05842e8313d018ac302536423a3e39ee5aeb7ff3
NAILA ZEMMACHE <155105829+nailamadjid123@users.noreply.github.com>
2024-11-01 18:01:16 +0100

    commit

--- exercice1.py
+++ exercice1.py
@@ -26,10 +26,32 @@ def calcul_entopie(source):
 # Fonction principale pour collecter les événements et probabilités
 
 def main():
-    source={}
-    n=int(input("Combien d'événements dans la source ???"))
- # Collecter les événements et leurs probabilités
-for i in range(n):
-    evenement=input(f"Nom de l'évenemnt {i+1}:")
 
-            
+    source = {} 
+
+    n = int(input("Combien d'événements dans la source ? "))  # Demander combien d'événements
+
+    
+    
+      
+
+           # Collecter les événements et leurs probabilités
+    for i in range(n):
+        evenement=input(f"Nom de l'évenemnt {i+1}:")
+        probabilité=float(input(f"probabilité de {evenement} :"))
+        source[evenement]=probabilité
+    
+ # Affichage des quantités d'information et calcul de l'entropie
+
+    print("\nQuantité d'information pour chaque événement:")
+
+    for evenement,probabilité in source.items():
+       information=quantité_information(probabilité)
+       print(f"{evenement}: {information:.4f} bits")
+
+    entropie = calcul_entopie(source)
+    print(f"\nEntropie totale de la source: {entropie:.4f} bits")
+
+            # Exécuter le programme
+if __name__ == "__main__":
+    main()
